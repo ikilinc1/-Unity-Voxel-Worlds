ba5ded2d3954c5c1175a6dc84a6f9074cebe37bf
ikilinc1 <ikilinc1@binghamton.edu>
2022-03-07 11:28:06 -0500

    Basic geometry builder with depth, height, and width

--- Voxel-Worlds/Assets/WorldController.cs
+++ Voxel-Worlds/Assets/WorldController.cs
@@ -5,24 +5,31 @@ using UnityEngine;
 public class WorldController : MonoBehaviour {
 
 	public GameObject block;
-	public int width = 1;
-	public int height = 1;
-	public int depth = 1;
+	public int width = 2;
+	public int height = 2;
+	public int depth = 2;
 	
 	public IEnumerator BuildWorld()
 	{
-		for(int z = 0; z < depth; z++)
-			for(int y = 0; y < height; y++)
+		for (int z = 0; z < depth; z++)
+		{
+			for (int y = 0; y < height; y++)
 			{
-				for(int x = 0; x < width; x++)
+				for (int x = 0; x < width; x++)
 				{
-					Vector3 pos = new Vector3(x,y,z);
-					GameObject cube = GameObject.Instantiate(block, pos, Quaternion.identity);
+					if (y >= height - 2 && Random.Range(0,100) < 50)
+					{
+						continue;
+					}
+					Vector3 position = new Vector3(x, y, z);
+					GameObject cube = GameObject.Instantiate(block, position, Quaternion.identity);
 					cube.name = x + "_" + y + "_" + z;
-					cube.GetComponent<Renderer>().material = new Material (Shader.Find("Standard"));
+					cube.GetComponent<Renderer>().material = new Material(Shader.Find("Standard"));
 				}
+				// yield after 1 row
 				yield return null;
 			}
+		}
 	}
 	// Use this for initialization
 	void Start () {
